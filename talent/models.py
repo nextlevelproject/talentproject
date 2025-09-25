ec76fa066f979c80cc6f3df3f040ec88d8e295ff
unknown <kendou@kakao.com>
2025-09-25 17:55:48 +0900

    250925

--- talent/models.py
+++ talent/models.py
@@ -6,3 +6,5 @@ class User(db.Model):
     userid = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    birthday = db.Column(db.DateTime, nullable=False)
+    tel_number = db.Column(db.String(120), unique=True, nullable=False)
