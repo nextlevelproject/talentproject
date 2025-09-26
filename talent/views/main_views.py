bc999f6c5bb0c4a8d8d06191e345d763bc168e3a
unknown <kendou@kakao.com>
2025-09-26 17:39:23 +0900

    250926

--- talent/views/main_views.py
+++ talent/views/main_views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    return 'Project Start!'
+    return render_template('index.html')
