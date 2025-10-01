12c2ab41f69992ca96dff620051e44794c380f9b
dny1010 <psy7363@gmail.com>
2025-10-01 09:19:09 +0900

    20250930

--- talent/__init__.py
+++ talent/__init__.py
@@ -4,25 +4,28 @@ from flask_migrate import Migrate
 from flask_mail import Mail
 
 import config
+from talent.views import store_views
 
 db = SQLAlchemy()
 migrate = Migrate()
 mail = Mail()
 
 def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app = Flask(__name__)
     app.config.from_object(config)
 
     # 익스텐션 초기화
     db.init_app(app)
     migrate.init_app(app, db)
     mail.init_app(app)
+    from . import models
 
     # 블루프린트 등록
-    from .views import main_views, community_views, auth_views
+    from .views import main_views, community_views, auth_views, store_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(community_views.bp)
     app.register_blueprint(auth_views.bp)
+    app.register_blueprint(store_views.bp)
 
     return app
 
