7ebc6e226ff6d99bb31aff62b453d5eeea1d0824
rlakeum93 <rlakeum93@gmail.com>
2025-10-02 17:00:04 +0900

    community final push

--- talent/views/community_views.py
+++ talent/views/community_views.py
@@ -1,15 +1,12 @@
-# community_views.py
 from flask import Blueprint, render_template, request, redirect, url_for, current_app
-from flask_login import current_user, login_user, logout_user, login_required
-from talent import db
-from talent.models import User, Post, Comment, Like, CommentLike
+from .models import db, User, Post, Comment, Like, CommentLike
+from flask_login import current_user, login_required
 from datetime import datetime, timedelta
 import pytz
 import os
 
-bp = Blueprint('community_views', __name__)
+bp = Blueprint('community', __name__, url_prefix='/community')
 
-# ----------------- Timezone -----------------
 KST = pytz.timezone('Asia/Seoul')
 
 def human_readable_time(post_time):
@@ -30,37 +27,25 @@ def human_readable_time(post_time):
     else:
         return post_local.strftime("%Y-%m-%d")
 
-# ----------------- Routes -----------------
-
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username, password=password).first()
-        if user:
-            login_user(user)
-            return redirect(url_for('community_views.home'))
-    return render_template('login.html')
-
-
-@bp.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('community_views.login'))
-
 
 @bp.route('/')
-def home():
-    posts = Post.query.order_by(Post.create_date.desc()).all()
-    hot_picks = Post.query.filter(Post.is_pro==True).limit(5).all()
-    return render_template('community.html',
-                           posts=posts,
-                           hot_picks=hot_picks,
-                           active_tab='community',
-                           human_readable_time=human_readable_time,
-                           current_user=current_user)
+@bp.route('/category/<string:category_name>')
+def home(category_name=None):
+    if category_name:
+        posts = Post.query.filter_by(category=category_name).order_by(Post.create_date.desc()).all()
+    else:
+        posts = Post.query.order_by(Post.create_date.desc()).all()
+
+    hot_picks = Post.query.filter(Post.is_pro == True).limit(5).all()
+    return render_template(
+        'community.html',
+        posts=posts,
+        hot_posts=hot_picks,
+        active_tab='community',
+        human_readable_time=human_readable_time,
+        current_user=current_user,
+        selected_category=category_name
+    )
 
 
 @bp.route('/create_post', methods=['GET', 'POST'])
@@ -69,6 +54,7 @@ def create_post():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
+        category = request.form.get('category')
         is_pro = 'is_pro' in request.form
         image = request.files.get('image')
         filename = None
@@ -76,14 +62,18 @@ def create_post():
             filename = f"{datetime.utcnow().timestamp()}_{image.filename}"
             image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
 
-        post = Post(title=title,
-                    content=content,
-                    is_pro=is_pro,
-                    image_filename=filename,
-                    author_id=current_user.id)
+        post = Post(
+            title=title,
+            content=content,
+            category=category,
+            is_pro=is_pro,
+            image_filename=filename,
+            user_id=current_user.id
+        )
         db.session.add(post)
         db.session.commit()
-        return redirect(url_for('community_views.home'))
+        return redirect(url_for('community.home'))
+
     return render_template('create_post.html')
 
 
@@ -93,15 +83,11 @@ def post_detail(post_id):
     if request.method == 'POST' and current_user.is_authenticated:
         comment_content = request.form.get('comment')
         if comment_content:
-            comment = Comment(content=comment_content,
-                              post_id=post.id,
-                              author_id=current_user.id)
+            comment = Comment(content=comment_content, post_id=post.id, user_id=current_user.id)
             db.session.add(comment)
             db.session.commit()
-            return redirect(url_for('community_views.post_detail', post_id=post.id))
-    return render_template('post_detail.html',
-                           post=post,
-                           human_readable_time=human_readable_time)
+            return redirect(url_for('community.post_detail', post_id=post.id))
+    return render_template('post_detail.html', post=post, human_readable_time=human_readable_time)
 
 
 @bp.route('/like_post/<int:post_id>', methods=['POST'])
@@ -112,7 +98,7 @@ def like_post(post_id):
     if not existing_like:
         db.session.add(Like(user_id=current_user.id, post_id=post.id))
         db.session.commit()
-    return redirect(url_for('community_views.post_detail', post_id=post.id))
+    return redirect(url_for('community.post_detail', post_id=post.id))
 
 
 @bp.route('/like_comment/<int:comment_id>', methods=['POST'])
@@ -123,7 +109,7 @@ def like_comment(comment_id):
     if not existing_like:
         db.session.add(CommentLike(user_id=current_user.id, comment_id=comment.id))
         db.session.commit()
-    return redirect(url_for('community_views.post_detail', post_id=comment.post_id))
+    return redirect(url_for('community.post_detail', post_id=comment.post_id))
 
 
 @bp.route('/pro_center')
