33fcfc94609385bd96ce3917dd94a8d786a32059
dny1010 <psy7363@gmail.com>
2025-10-10 11:05:53 +0900

    20251010

--- talent/views/main_views.py
+++ talent/views/main_views.py
@@ -7,4 +7,4 @@ bp = Blueprint('main', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     posts = CommunityPost.query.order_by(CommunityPost.create_date.desc()).limit(3).all()
-    return render_template('main/main.html')
+    return render_template('main.html')
