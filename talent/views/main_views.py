8070bc4f3d45da98d20b6a6f6373530df4464a98
dny1010 <psy7363@gmail.com>
2025-10-13 09:48:48 +0900

    20251012

--- talent/views/main_views.py
+++ talent/views/main_views.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template
-
-from ..models import CommunityPost
+from sqlalchemy import func
+from talent.models import Store, CommunityPost
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    posts = CommunityPost.query.order_by(CommunityPost.create_date.desc()).limit(3).all()
-    return render_template('main.html')
+    like_count = func.count().label("like_count")
+
+    s_posts = (Store.query.order_by(Store.views.desc(), Store.create_date.desc()).limit(3).all())
+    c_posts = (CommunityPost.query.outerjoin(CommunityPost.likes).group_by(CommunityPost.id)
+               .order_by(like_count.desc(), CommunityPost.create_date.desc()).limit(3).all())
+    return render_template('main.html', s_posts=s_posts, c_posts=c_posts)
